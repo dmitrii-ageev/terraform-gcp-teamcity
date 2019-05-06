6efba327d64701f6a87b181f86e407573ab10329
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-05-07 10:14:34 +1200

    Remove kubernetes_service data source

--- data.tf
+++ data.tf
@@ -1,11 +1,3 @@
 data "google_dns_managed_zone" "teamcity" {
   name = "${var.dns_managed_zone}"
 }
-
-data "kubernetes_service" "teamcity" {
-  provider = "kubernetes"
-
-  metadata {
-    name = "${local.service_name}"
-  }
-}
--- locals.tf
+++ locals.tf
@@ -1,5 +1,4 @@
 locals {
   hostname         = "${var.dns_managed_zone != "" ? format("%s.%s", var.service_name, replace(data.google_dns_managed_zone.teamcity.dns_name, "/.$/", "")) : var.ingress_host}"
   service_name     = "${helm_release.teamcity.name}-${var.service_name}"
-  load_balancer_ip = "${data.kubernetes_service.teamcity.load_balancer_ingress.0.ip}"
 }
