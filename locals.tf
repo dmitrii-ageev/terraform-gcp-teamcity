107bc4d06eca99c89a29aa716afeb25d1d247180
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-05-07 13:34:46 +1200

    Leave ingress_host blank if not defined

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  hostname         = "${var.dns_zone_name != "" ? format("%s.%s", var.service_name, replace(var.dns_zone_name, "/.$/", "")) : local.ingress_host}"
+  host_ip          = "${google_compute_global_address.teamcity.address}"
+  hostname         = "${var.dns_zone_name != "" ? format("%s.%s", var.service_name, replace(var.dns_zone_name, "/.$/", "")) : local.host_ip}"
   service_name     = "${helm_release.teamcity.name}-${var.service_name}"
-  ingress_host     = "${var.ingress_host == "" ? "*/*" : var.ingress_host}"
 }
