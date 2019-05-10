00e475f08439f204543d4210e9c6c93a96e3934b
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-05-10 15:48:05 +1200

    Remove "cluster*" local variables

--- locals.tf
+++ locals.tf
@@ -3,7 +3,4 @@ locals {
   hostname         = "${var.dns_zone_name != "" ? format("%s.%s", var.service_name, replace(var.dns_zone_name, "/.$/", "")) : local.host_ip}"
   service_name     = "${helm_release.teamcity.name}-${var.service_name}"
   ingress_host     = "${var.dns_zone_name == "" ? "" : local.hostname}"
-  cluster_token    = "${var.gke_token}"
-  cluster_endpoint = "${var.gke_endpoint}"
-  cluster_ca_certificate = "${var.gke_ca_certificate}"
 }
