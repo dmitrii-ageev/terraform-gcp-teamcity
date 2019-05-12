9feba4bcf84822328d74e14797683b74ea8278c8
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-05-13 10:26:50 +1200

    Change the helm chart version

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   helm_chart_name    = "unimarket-helm-teamcity"
-  helm_chart_version = "1.0.2"
+  helm_chart_version = "1.0.3"
   helm_chart         = "${local.helm_chart_name}-${local.helm_chart_version}"
   helm_chart_file    = "${path.root}/../helm-charts/${local.helm_chart}.tar.gz"
   host_ip            = "${google_compute_global_address.teamcity.address}"
