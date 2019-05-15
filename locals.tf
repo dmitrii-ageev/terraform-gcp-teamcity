9c92f5ea44212083f5ff7c057b7c18902fb063e5
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-05-16 09:50:05 +1200

    Update the chart source

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   helm_chart_name    = "unimarket-helm-teamcity"
   helm_chart_version = "1.0.7"
-  helm_chart         = "git::ssh://git@github.com/Unimarket/${local.helm_chart_name}.git?ref=${local.helm_chart_version}"
+  helm_chart         = "https://github.com/dmitrii-ageev/helm-teamcity/archive/1.0.7.tar.gz"
   host_ip            = "${google_compute_global_address.teamcity.address}"
   hostname           = "${var.dns_zone_name != "" ? format("%s.%s", var.service_name, replace(var.dns_zone_name, "/.$/", "")) : local.host_ip}"
   service_name       = "${helm_release.teamcity.name}-${var.service_name}"
