7dd833f10fff725391fbb2566e8bfe61a613a76f
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-07-05 14:45:32 +1200

    Latest changes

--- provider.tf
+++ provider.tf
@@ -1,16 +1,10 @@
 provider "helm" {
-  install_tiller  = true
-  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.11.0"
-  service_account = "tiller"
-
-  kubernetes {
-    token                  = "${var.gke_token}"
-    host                   = "${var.gke_endpoint}"
-    cluster_ca_certificate = "${base64decode(var.gke_ca_certificate)}"
-  }
+  version        = "0.9.0"
+  install_tiller = false
 }
 
 provider "kubernetes" {
+  version                = "1.6.0"
   token                  = "${var.gke_token}"
   host                   = "${var.gke_endpoint}"
   cluster_ca_certificate = "${base64decode(var.gke_ca_certificate)}"
